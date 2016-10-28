256686ab62f72a0fcac10359a7fca30fd0283166
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-10-28 07:48:04 -0700

    Add README content, start server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1. `npm install`
-2. `npm run dev-server`
+2. `npm run dev-server` for the webpack dev server or `npm run
+   front-end` to build the `bundle.js`
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+const port = 8080;
+
 app.use(express.static(`${__dirname}/public`));
+
+app.listen(port, () => console.log(`Started on ${port}`));
