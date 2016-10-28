7eb0d497859e2f3cf278d7eeb1c57dc9414421f4
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-10-28 16:17:32 -0700

    Remove env var

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // const builder = require('botbuilder');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 80;
+const port = 8080;
 const path = require('path');
-const http = require('http');
 
 // const connector = new builder.ChatConnector({
 //   appId: '',
@@ -24,7 +23,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.post('/api/messages', connector.listen());
 // app.get('/api/conversations', connector.listen());
 
-
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(port, () => console.log(`Started on ${port}`));
