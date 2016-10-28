250596e680c7b6da5903c8204af868e420796c85
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-10-28 15:04:47 -0700

    Remove fin advisor, use iframe from bot

--- lib/financeAdvisor.jsx
+++ lib/financeAdvisor.jsx
@@ -4,127 +4,22 @@ const { Component } = React;
 
 export default
 class FinanceAdvisor extends Component {
-  constructor() {
-        super();
-        this.clickHandler = this.clickHandler.bind(this);
-        this.state = {chatDefault: false}
-      }
-
-      clickHandler(e) {
-           this.setState({...this.state, chatDefault: !this.state.chatDefault});
-         }
-render () {
-const activeStyle = {
-    mainContainer: {
-      height: '40%',
-      width: '20%',
-      backgroundColor: 'powderblue',
-      opacity: '.90',
-      flexDirection: 'column',
-      zIndex: '10000',
-      bottom: '0',
-      left: '70%',
-      right: '10%',
-      position: 'fixed',
-      borderRadius: '5px'
-    },
-    header: {
-      display: 'flex',
-      backgroundColor: 'grey',
-      color: 'white',
-      font: 'Sans-serif',
-      paddingTop: '.25em',
-      paddingBottom: '.25em',
-      width: '100%',
-      height: '12%',
-      fontSize: '2em',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: '5px'
-    },
-    inputDiv: {
-      position: 'fixed',
-      bottom: '0',
-      height: '2em',
-      width: '20%',
-      display: 'flex'
-
-    },
-    input: {
-      border: 'none',
-      height: '2em',
-      width: '100%',
-      paddingLeft: '.25em'
-    },
-    sendMsg: {
-      position: 'absolute',
-      right: '0',
-      backgroundColor: '#83D3C4',
-      border: 'none',
-      height: '2em',
-      width: '3.5em'
-    },
-  };
-  const styles = {
-    mainContainer: {
-
-      width: '20%',
-
-
-
-      zIndex: '10000',
-      bottom: '0',
-      left: '70%',
-      right: '10%',
-      position: 'fixed',
-      borderRadius: '5px'
-    },
-    header: {
-      display: 'flex',
-      backgroundColor: 'grey',
-      color: 'white',
-      font: 'Sans-serif',
-      paddingTop: '.25em',
-      paddingBottom: '.25em',
-      width: '100%',
-      height: '12%',
-      fontSize: '2em',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: '5px'
-    },
-    inputDiv: {
-    display: 'none'
-
-    },
-    input: {
-      display: 'none'
-    },
-    sendMsg: {
-      display: 'none'
-    },
-  }
-  let currentStyle = this.state.chatDefault ? activeStyle : styles;
+  render () {
+    const botSecret = 'j8wZqP9tO4k.cwA.mxE.Zl9qLEbnDTwPHK-nqHpUU-avrdNq-x2Gr-sOXZkVL9g';
+    const link =
+	  `https://webchat.botframework.com/embed/finance-advisor?s=${botSecret}`;
+    const botStyle = {
+      height:'400px',
+      maxHeight:'400px'
+    };
+    const wrapperStyle = {
+      position:'absolute',
+      bottom:0,
+      right:0
+    };
     return (
-      <div style = {currentStyle.mainContainer}
-        >
-
-        <div
-          style = {currentStyle.header}
-          onClick={this.clickHandler}>
-          Advisor Bot
-        </div>
-        <div style = {currentStyle.inputDiv}>
-
-          <button style={currentStyle.sendMsg}>
-            Send
-          </button>
-          <input
-            style = {currentStyle.input}
-            type={'text'}
-            placeholder={'How can I help?'}
-          />
-        </div>
+      <div style={wrapperStyle}>
+	<iframe src={link} style={botStyle}></iframe>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+// const builder = require('botbuilder');
 const express = require('express');
 const app = express();
+const port = 80;
 
-const port = 8080;
+// const connector = new builder.ChatConnector({
+//   appId: '',
+//   appPassword: ''
+// });
+
+// const bot = new builder.UniversalBot(connector);
 
 app.use(express.static(`${__dirname}/public`));
+
+// bot.dialog('/', function (session) {
+//   console.log('called');
+//   console.log(session);
+//   session.send("Hello World");
+// });
+
+// app.post('/bot-message', connector.listen());
+// app.post('/api/messages', connector.listen());
+// app.get('/api/conversations', connector.listen());
 
 app.listen(port, () => console.log(`Started on ${port}`));
