b86b90c6f922cf7a698d64aada407b89e227d0fd
Vianka Lemus <vianka.lemus@gmail.com>
2016-10-28 07:59:09 -0700

    div and message commands

--- lib/financeAdvisor.jsx
+++ lib/financeAdvisor.jsx
@@ -4,10 +4,55 @@ const { Component } = React;
 
 export default
 class FinanceAdvisor extends Component {
+  //
+  // constructor() {
+  //   super();
+  //   this.state = { msgs: [], command:''};
+  //   this.conn = new WebSocket(webSocketAddr);
+  //   this.getCommand = this.getCommand.bind(this);
+  //   this.cleanCommand = this.cleanCommand.bind(this);
+  // }
+  // componentDidMount() {
+  //   this.conn.onmessage = (message) => {
+  //     const reply = JSON.parse(message.data);
+  //     switch (reply.message_type) {
+  //       case 'new_chat_message':
+  //       this.setState({ msgs: [...this.state.msgs] });
+  //       break;
+  //       default:
+  //       console.error('Oops, Cortana is confused');
+  //     }
+  //   };
+  //   const initialMessageSendTimer = setInterval(() =>{
+  //     if (this.conn.readyState === 1) {
+  //       this.conn.send(JSON.stringify({
+  //         cmd: 'connect',
+  //       }));
+  //       clearInterval(initialMessageSendTimer);
+  //     }
+  //   }, 500);
+  //   getCommand() {
+  //     this.setState({command:'Hi Cortana,'});
+  //   }
+  //   cleanCommand(){
+  //     this.setState({command:''});
+  //   }
+  // }
   render () {
+    let mainContainer = {
+      height: '30em',
+      width: '20em',
+      backgroundColor: 'black',
+    }
+    let header = {
+      backgroundColor: 'grey',
+      width: '100%',
+      height: '3em'
+    }
     return (
-      <div>
-	Advisor Bot
+      <div style ={mainContainer}>
+        <div style = {header}></div>
+Advisor Bot
       </div>
     );
   }
