ae75a2ca5a2cd7aa69fbd2c81111a9b816585bca
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-10-28 23:15:44 -0700

    Try https server

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
-// const builder = require('botbuilder');
+const builder = require('botbuilder');
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 8080;
 const path = require('path');
+const https = require('https');
 
-// const connector = new builder.ChatConnector({
-//   appId: '',
-//   appPassword: ''
-// });
+try {
 
-// const bot = new builder.UniversalBot(connector);
+  var creds = {
+    key : fs.readFileSync('./server.key', 'utf8'),
+    cert : fs.readFileSync('./server.crt', 'utf8')
+  };
+} catch (e) {
+  var creds = {};
+  console.log('Not an error if running locally');
+}
+
+const connector = new builder.ChatConnector({
+  appId: '',
+  appPassword: ''
+});
+
+const bot = new builder.UniversalBot(connector);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// bot.dialog('/', function (session) {
-//   console.log('called');
-//   console.log(session);
-//   session.send("Hello World");
-// });
 
-// app.post('/bot-message', connector.listen());
-// app.post('/api/messages', connector.listen());
-// app.get('/api/conversations', connector.listen());
+bot.dialog('/', function (session) {
+  console.log('called');
+  console.log(session);
+  session.send("Hello World");
+});
+
+app.post('/bot-message', connector.listen());
+app.post('/api/messages', connector.listen());
+app.get('/api/conversations', connector.listen());
 
 app.listen(port, () => console.log(`Started on ${port}`));
+https.createServer(creds, app)
+  .listen(443, () => console.log('Started https server'));
